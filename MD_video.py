86a9a09c77c22ba921f169c4fd7cb62442a68546
IAXE <36336432+IAXE@users.noreply.github.com>
2018-02-14 07:36:17 -0500

    Update MD_video.py

--- MD_video.py
+++ MD_video.py
@@ -1,21 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 11 03:36:11 2018
 
-@author: eddy
-"""
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('Hachiko.mkv')
+cap = cv2.VideoCapture('Pelicula.mkv')
 print(cap.isOpened())
 
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
  
-    cv2.imshow('Pelicula_Hachiko',frame)
+    cv2.imshow('Pelicula',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
